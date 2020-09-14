42d579d3a2a16c62b81b093e015f360e7ddced9c
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-09-13 23:52:13 -0500

    re-styled contact page

---
+++ script.js
@@ -0,0 +1,10 @@
+$('#gitLink').click(function(){
+
+    window.open('https://github.com/LiliCecilia23', '_blank'); 
+
+});
+$('#liLink').click(function(){
+
+    window.open('https://www.linkedin.com/in/lili-clift-a530bb162/', '_blank'); 
+
+});
