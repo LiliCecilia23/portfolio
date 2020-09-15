ea2328b7a885ce2739224cff8507b07b029959c5
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-09-15 16:14:11 -0500

    finished js

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+//home page links
 $('#gitLink').click(function(){
-
     window.open('https://github.com/LiliCecilia23', '_blank'); 
-
 });
 $('#liLink').click(function(){
-
     window.open('https://www.linkedin.com/in/lili-clift-a530bb162/', '_blank'); 
+});
 
+//portolio links
+$('#code1').click(function(){
+    window.open('https://github.com/LiliCecilia23/WatchIt', '_blank'); 
+});
+$('#page1').click(function(){
+    window.open('https://smrodriguez88.github.io/WatchIt/', '_blank'); 
+});
+$('#code2').click(function(){
+    window.open('https://github.com/LiliCecilia23/weather-dashboard', '_blank'); 
+});
+$('#page2').click(function(){
+    window.open('https://lilicecilia23.github.io/weather-dashboard/', '_blank'); 
+});
+$('#code3').click(function(){
+    window.open('https://github.com/LiliCecilia23/work-day-scheduler', '_blank'); 
+});
+$('#page3').click(function(){
+    window.open('https://lilicecilia23.github.io/work-day-scheduler/', '_blank'); 
+});
+$('#code4').click(function(){
+    window.open('https://github.com/LiliCecilia23/password-generator', '_blank'); 
+});
+$('#page4').click(function(){
+    window.open('https://lilicecilia23.github.io/password-generator/', '_blank'); 
 });
